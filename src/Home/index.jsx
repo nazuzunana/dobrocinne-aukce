579a215c5873143564cbec8547bc8116f69964ab
Zuzana Červínová <zuzankacervinova@gmail.com>
2022-05-27 20:23:21 +0200

    paddingy homepage

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -30,7 +30,7 @@ const Home = () => {
           <p>
             NGO DEI (我地) je nezisková organizace se sídlem v Praze, kterou
             založila skupina Čechů a Hongkongců. Znaky 我地 znamenají v
-            kantonštině "my" nebo "moje země". Iniciály DEI znamenají v
+            kantonštině „my“ nebo „moje země“. Iniciály DEI znamenají v
             anglickém překladu Diversity, Equality, and Inclusion (rozmanitost,
             rovnost a začlenění).
           </p>
