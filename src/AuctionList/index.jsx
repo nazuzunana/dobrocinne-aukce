bd238f155cb9f96c2b5412d7bdf0d09ef8367cd3
Viktoria Andrascikova <viktoria.andrascikova@gmail.com>
2022-05-29 23:03:40 +0200

    oprava javascriptu v komponente auction list

--- src/AuctionList/index.jsx
+++ src/AuctionList/index.jsx
@@ -3,6 +3,7 @@ import './style.css';
 import { Link } from 'react-router-dom';
 import timer from './img/timer.svg';
 import imageRadojcic from './img/Radojcic_zahrada_1.jpg';
+import imageSen from './img/prazsky_sen.jpg';
 
 // seznam aukcí
 
@@ -15,7 +16,7 @@ const auctions = [
   },
 
   {
-    img: '',
+    img: imageSen,
     title: 'Aukce na podporu psího útulku',
     description:
       'LOREM ipsum dolor sit amet consectetur adipisicing elit. Ipsa error delectus nulla quaerat, voluptatum excepturi expedita dolores ab id dolor corporis. Počet předmětů: 2',
@@ -27,7 +28,9 @@ const auctions = [
 const Auction = ({ img, title, description }) => (
   <>
     <section className="container__list">
-      <div className="auction__img"></div>
+      <div>
+        <img className="auction__img" src={img} />
+      </div>
       <div className="auction__content">
         <h2>{title}</h2>
         <div className="auction__description">{description}</div>
