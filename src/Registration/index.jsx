10eec76ac86726999a29601655cac8d148ca75c0
Viktoria Andrascikova <viktoria.andrascikova@gmail.com>
2022-05-29 14:20:46 +0200

    pridanie validatoru do formulara

--- src/Registration/index.jsx
+++ src/Registration/index.jsx
@@ -1,13 +1,37 @@
 import React, { useState } from 'react';
 import './style.css';
+import validator from 'validator';
 
 // registrační formulář
 
 const Registration = () => {
   const [userName, setUserName] = useState('');
   const [userLastName, setUserLastName] = useState('');
-  const [userEmail, setUserEmail] = useState('');
   const [userPhone, setUserPhone] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  const validateEmail = (e) => {
+    const email = e.target.value;
+
+    // validátor hesla - fukčný :)
+
+    if (validator.isEmail(email)) {
+      setEmailError('');
+    } else {
+      setEmailError('zadejte platný e-mail');
+    }
+  };
+
+  const validatePassword = (e) => {
+    const password = e.target.value;
+
+    if (validator.isStrongPassword(password)) {
+      setPasswordError('silné heslo');
+    } else {
+      passwordError('není dostatečně silné heslo');
+    }
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -16,14 +40,45 @@ const Registration = () => {
   };
   const jeUserNamePrazdne = userName.length === 0;
   const jeLastNamePrazdne = userLastName.length === 0;
-  const jeUserEmail = userEmail.length === 0;
-  const jeUserPhone = userEmail.length === 0;
+  const jeUserPhone = userPhone.length === 0;
 
   return (
     <div className="container__registration">
       <div className="">Už u nás máte účet? Přihlaste se tady.</div>
       <form onSubmit={handleSubmit}>
         <h3 className="registration__title">Registrace</h3>
+        <div className="form_container">
+          <h4 className="">Přihlašovací údaje</h4>
+          <label>
+            <span>E-mail: </span>
+            <input type="email" onChange={(e) => validateEmail(e)}></input>{' '}
+            <p
+              style={{
+                color: 'red',
+              }}
+            >
+              {emailError}
+            </p>
+          </label>
+          <label>
+            Heslo:
+            <input
+              type="password"
+              onChange={(e) => validatePassword(e)}
+            ></input>{' '}
+            <p
+              style={{
+                color: 'red',
+              }}
+            >
+              {passwordError}
+            </p>
+          </label>
+          <label>
+            Heslo znovu:
+            <input type="password" />
+          </label>
+        </div>
         <div className="form_container">
           <h4 className="">Osobní údaje</h4>
           <label>
@@ -53,20 +108,6 @@ const Registration = () => {
           </label>
 
           {jeLastNamePrazdne && <div>Příjmení je povinný údaj.</div>}
-          <label>
-            E-mail:
-            <input
-              type="email"
-              value={userEmail}
-              onChange={(event) => {
-                const vstup3 = event.target.value;
-                console.log(vstup3);
-                setUserEmail(vstup3);
-              }}
-            />
-          </label>
-
-          {jeUserEmail && <div>E-mail je povinný údaj.</div>}
           <label>
             Příjmení:
             <input
