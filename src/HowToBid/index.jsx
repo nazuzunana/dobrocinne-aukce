c117905c3d1191f062915ca7c5d8b877383a4aac
Zuzana Červínová <zuzankacervinova@gmail.com>
2022-06-02 10:05:53 +0200

    jak dražit: text

--- src/HowToBid/index.jsx
+++ src/HowToBid/index.jsx
@@ -35,7 +35,49 @@ const HowToBid = () => {
           </div>
         </div>
       </div>
-      <div className="how to bid__description"></div>
+      <div className="how-to-bid__description">
+        <div className="description__article">
+          <div className="description__title">
+            <p>Podmínky přihazování</p>
+          </div>
+          <div className="description__text">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Reprehenderit, placeat fuga quos omnis accusamus explicabo
+              voluptate! Quaerat ullam consequatur eveniet nihil magnam! Fugit
+              quos officia molestias dolorum vero eveniet necessitatibus?
+            </p>
+          </div>
+        </div>
+        <div className="description__article">
+          <div className="description__title">
+            <p>Platba za vydražené předměty</p>
+          </div>
+          <div className="description__text">
+            <p>
+              Po vydražení předmětu obdržíte na e-mailovou adresu, kterou jste
+              uvedli při registraci, email s fakturou a instrukcemi k platbě.
+            </p>
+          </div>
+        </div>
+        <div className="description__article">
+          <div className="description__title">
+            <p>Převzetí vydražených předmětů</p>
+          </div>
+          <div className="description__text">
+            <p>
+              Vydražené předměty si je po uhrazení faktury možné vyzvednout
+              osobně na adrese: Letohradská 711/10, 170 00 Praha 7-Holešovice
+              během otevírací doby: Út - Ne: 11:00 - 21:00 či jindy po dohodě na
+              tel: +420 606 125 203 či na e-mail: info@ngodei.org .
+              <br />
+              <br />
+              Po domluvě lze vydražené předměty zaslat. Výběr dopravce a cena
+              dopravy závisí na velikosti a povaze jednotlivých předmětů.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
