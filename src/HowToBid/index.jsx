6ab0266d1c7f74365ccf217169b49bd6739b0d1e
Zuzana Červínová <zuzankacervinova@gmail.com>
2022-06-02 09:41:46 +0200

    jak dražit: kroky

--- src/HowToBid/index.jsx
+++ src/HowToBid/index.jsx
@@ -6,19 +6,33 @@ import './style.css';
 const HowToBid = () => {
   return (
     <div className="container__how-to-bid">
-      <div className="how-to-bid__title">Jak dražit</div>
       <div className="how-to-bid__steps">
-        <div className="how-to-bid_step">
-          <div className="step_icon"></div>
-          <div className="step_instructions"></div>
+        <div className="how-to-bid__step">
+          <div className="how-to-bid__icon form"></div>
+          <div className="step_instructions">
+            <p>
+              Pro možnost přihazovat na předměty v aukcích si založte
+              uživatelský učet.
+            </p>
+          </div>
         </div>
-        <div className="how-to-bid_step">
-          <div className="step_icon"></div>
-          <div className="step_instructions"></div>
+        <div className="how-to-bid__step">
+          <div className="how-to-bid__icon mail"></div>
+          <div className="step_instructions">
+            <p>
+              Vyčkejte na potvrzovací e-mail a potvrďte svoji registraci
+              kliknutím na odkaz v e-mailu.
+            </p>
+          </div>
         </div>
-        <div className="how-to-bid_step">
-          <div className="step_icon"></div>
-          <div className="step_instructions"></div>
+        <div className="how-to-bid__step">
+          <div className="how-to-bid__icon login"></div>
+          <div className="step_instructions">
+            <p>
+              Po založení uživatelského účtu a potvrzení registrace se
+              přihlaste.
+            </p>
+          </div>
         </div>
       </div>
       <div className="how to bid__description"></div>
