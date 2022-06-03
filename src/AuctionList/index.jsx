ae70e34c406f27d2c99da0cc9f88d9221b4d8a5e
Zuzana Červínová <zuzankacervinova@gmail.com>
2022-06-03 11:51:13 +0200

    seznam aukcí: rozmístění a responzivita

--- src/AuctionList/index.jsx
+++ src/AuctionList/index.jsx
@@ -9,14 +9,16 @@ import imageSen from './img/prazsky_sen.jpg';
 
 const auctions = [
   {
-    img: imageRadojcic,
+    // img: imageRadojcic,
+    img: 'img__radojcic',
     title: 'Aukce na pomoc Ukrajině',
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa error delectus nulla quaerat, voluptatum excepturi expedita dolores ab id dolor corporis. Počet předmětů: 2',
   },
 
   {
-    img: imageSen,
+    // img: imageSen,
+    img: 'img__sen',
     title: 'Aukce na podporu psího útulku',
     description:
       'LOREM ipsum dolor sit amet consectetur adipisicing elit. Ipsa error delectus nulla quaerat, voluptatum excepturi expedita dolores ab id dolor corporis. Počet předmětů: 2',
@@ -27,9 +29,10 @@ const auctions = [
 
 const Auction = ({ img, title, description }) => (
   <>
-    <section className="container__list">
+    <section className="auction-list__auction">
       <figure>
-        <img className="auction__img" src={img} />
+        {/* <img className="auction__img" src={img} /> */}
+        <div className={img}></div>
       </figure>
       <div className="auction__content">
         <h2>{title}</h2>
@@ -44,7 +47,7 @@ const Auction = ({ img, title, description }) => (
 );
 
 const AuctionList = () => (
-  <div>
+  <div className="container__auction-list">
     {auctions.map((auction) => (
       <Auction
         img={auction.img}
