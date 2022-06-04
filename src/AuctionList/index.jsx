3fdd59318881a54d1273594172b6e67251ce0b0a
Zuzana Červínová <zuzankacervinova@gmail.com>
2022-06-04 12:30:22 +0200

    seznam aukcí ze storage

--- src/AuctionList/index.jsx
+++ src/AuctionList/index.jsx
@@ -1,56 +1,86 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
 import { Timer } from '../Timer';
 import { Link } from 'react-router-dom';
+import { collection, getDocs, orderBy, query } from 'firebase/firestore';
+import { db, storage } from '../firebase';
+import { ref, getDownloadURL } from 'firebase/storage';
 
 // seznam aukcí
 
-export const auctionListAuctions = [
-  {
-    img: 'auction__img img__pristav',
-    title: 'Aukce na pomoc Ukrajině',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa error delectus nulla quaerat, voluptatum excepturi expedita dolores ab id dolor corporis. Počet předmětů: 2',
-  },
+// export const auctionListAuctions = [
+//   {
+//     img: 'auction__img img__pristav',
+//     title: 'Aukce na pomoc Ukrajině',
+//     description:
+//       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa error delectus nulla quaerat, voluptatum excepturi expedita dolores ab id dolor corporis. Počet předmětů: 2',
+//   },
 
-  {
-    img: 'auction__img img__zahrada',
-    title: 'Aukce na podporu psího útulku',
-    description:
-      'LOREM ipsum dolor sit amet consectetur adipisicing elit. Ipsa error delectus nulla quaerat, voluptatum excepturi expedita dolores ab id dolor corporis. Počet předmětů: 2',
-  },
-];
+//   {
+//     img: 'auction__img img__zahrada',
+//     title: 'Aukce na podporu psího útulku',
+//     description:
+//       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa error delectus nulla quaerat, voluptatum excepturi expedita dolores ab id dolor corporis. Počet předmětů: 2',
+//   },
+// ];
 
-// hore je pole/zoznam aukcií, tam sa pridáva ďalšia aukcia
+const AuctionListAuction = ({ img, title, description }) => {
+  const [imageUrl, setImageUrl] = useState('');
 
-const AuctionListAuction = ({ img, title, description }) => (
-  <>
-    <section className="auction-list__auction">
-      <div className={img}></div>
-      <div className="auction__content">
-        <h2>{title}</h2>
-        <div className="auction__description">{description}</div>
-        <Timer />
-        <Link to="/Auction">
-          <button className="auction__enter">Přejít do aukce</button>
-        </Link>
-      </div>
-    </section>
-  </>
-);
+  useEffect(() => {
+    getDownloadURL(ref(storage, img[0])).then((url) => setImageUrl(url));
+  }, [img, setImageUrl]);
 
-const AuctionList = () => (
-  <div className="container__auction-list">
-    {auctionListAuctions.map((auctionListAuction) => (
-      <AuctionListAuction
-        key={auctionListAuction.title}
-        img={auctionListAuction.img}
-        title={auctionListAuction.title}
-        description={auctionListAuction.description}
-      />
-    ))}
-  </div>
-);
+  return (
+    <>
+      <section className="auction-list__auction">
+        <div
+          className="auction__img"
+          style={{
+            backgroundImage: `url(${imageUrl})`,
+          }}
+        ></div>
+        <div className="auction__content">
+          <h2>{title}</h2>
+          <div className="auction__description">{description}</div>
+          <Timer />
+          <Link to="/Auction">
+            <button className="auction__enter">Přejít do aukce</button>
+          </Link>
+        </div>
+      </section>
+    </>
+  );
+};
+
+const AuctionList = () => {
+  const [auctionListAuctions, setAuctionListAuctions] = useState([]);
+
+  useEffect(() => {
+    getDocs(query(collection(db, 'auctions'), orderBy('endDate', 'desc'))).then(
+      (querySnapshot) => {
+        const auctions = [];
+        querySnapshot.forEach((doc) => {
+          auctions.push({ id: doc.id, ...doc.data() });
+        });
+        setAuctionListAuctions(auctions);
+      },
+    );
+  }, [setAuctionListAuctions]);
+
+  return (
+    <div className="container__auction-list">
+      {auctionListAuctions.map((auctionListAuction) => (
+        <AuctionListAuction
+          key={auctionListAuction.id}
+          img={auctionListAuction.img}
+          title={auctionListAuction.title}
+          description={auctionListAuction.description}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default AuctionList;
 
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,4 +1,12 @@
 import { initializeApp } from 'firebase/app';
 import { firebaseConfig } from './config';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
+
+// Get a reference to the storage service, which is used to create references in your storage bucket
+export const storage = getStorage(app);
